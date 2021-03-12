a1b9f7fef4390d914ebd3eec2d1981da33848d6f
raphael.alabi <raphael.alabi@docusign.com>
2021-03-12 13:25:57 -0800

    Revert "Merge pull request #1 from ishanUNC/main"
    This reverts commit 8c76e3fdd6807f76e7d0dbde6a20c8167024b766, reversing
    changes made to 15cc986c75547ba911dd22ed1775a7ad44750ba0.
    
    Way too many conflicts and the commit has not been tested for integration

--- src/app/main.py
+++ src/app/main.py
@@ -27,19 +27,15 @@ import os
 import cv2
 import numpy as np
 import logging
-import copy
 import datetime
-
-import pickle
-from scipy.io import wavfile
-import sys
-from coughvid.DSP import classify_cough
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('file_error.log')
 file_handler.setLevel(logging.ERROR)
 
 app=flask.Flask(__name__,template_folder="jinja_templates")
+
+
 
 @app.route('/upload', methods=['GET'])
 def uploading():
@@ -77,14 +73,12 @@ def upload():
 
     print('This is our predicted result ...... {}'.format(results))
     probability = results[0][0]
-    if probability < 0.0001:
-        return render_template('negative_response.html',probability='less than 0.01')
-    elif probability < 0.2:
+    if probability < 0.2:
         # non-covid patient lets return page for  non-covid
-        return render_template('negative_response.html',probability=str(round(probability*100, 2)))
+        return render_template('negative_response.html',probability=probability)
     else:
         # likely a covid patient
-        return render_template('positive_response.html',probability=str(round(probability*100, 2)))
+        return render_template('positive_response.html',probability=probability)
 
 
 
@@ -93,17 +87,6 @@ def preprocessing(waveform_file):
 
     ''' Takes in a wave file and returns the mel frequency and the mel spectogram '''
     audio, sr = librosa.load(waveform_file)
-    waveform_file.stream.seek(0)
-    
-    ## Validate Cough
-    sr1, audio1 = wavfile.read(waveform_file)
-    prob_cough = validate_cough(audio1, sr1)
-
-    if prob_cough < 0.5:
-        raise ValueError("""The audio was not detected to be a cough. Please
-                          use a different file or re-record the cough.  
-                          Try to remove background noise if possible.""")
-
     mfccs = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=39)
     mfccs_scaled = np.mean(mfccs.T, axis=0)
     mfccs_1 = np.expand_dims(mfccs_scaled, axis=0)
@@ -134,15 +117,7 @@ def preprocessing(waveform_file):
     final = [mfccs_1,image_1,sample_2]
     return final
 
-def validate_cough(audio, sr):
-    model_dir = '../data'
-    with open (f'{model_dir}/cough_classifier', 'rb') as pickle_f:
-        detection_model = pickle.load(pickle_f)
-    with open (f'{model_dir}/cough_classification_scaler', 'rb') as pickle_f2:
-        detection_scaler = pickle.load(pickle_f2)
-    prob_cough = classify_cough(audio, sr, detection_model, detection_scaler)
-    print(f"The file has a {round(prob_cough*100, 2)}% probability of being a cough.")
-    return prob_cough
+
 
 
 if __name__ == '__main__':

b8cccd2b828ade70c90d1d4de70153ebc06c1df4
Ishan Shah <ijshah14@gmail.com>
2021-03-14 12:14:03 -0700

    Adding validation code

--- src/app/main.py
+++ src/app/main.py
@@ -27,7 +27,13 @@ import os
 import cv2
 import numpy as np
 import logging
+import copy
 import datetime
+
+import pickle
+from scipy.io import wavfile
+import sys
+from coughvid.DSP import classify_cough
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('file_error.log')
@@ -35,14 +41,11 @@ file_handler.setLevel(logging.ERROR)
 
 app=flask.Flask(__name__,template_folder="jinja_templates")
 
-
-
 @app.route('/upload', methods=['GET'])
 def uploading():
 
     response = flask.Response(render_template('upload.html'))
     return response
-
 
 
 @app.route('/coughsound', methods=['POST'])
@@ -73,20 +76,31 @@ def upload():
 
     print('This is our predicted result ...... {}'.format(results))
     probability = results[0][0]
-    if probability < 0.2:
+    if probability < 0.0001:
+        return render_template('negative_response.html',probability='less than 0.01')
+    elif probability < 0.2:
         # non-covid patient lets return page for  non-covid
-        return render_template('negative_response.html',probability=probability)
+        return render_template('negative_response.html',probability=str(round(probability*100, 2)))
     else:
         # likely a covid patient
-        return render_template('positive_response.html',probability=probability)
-
-
+        return render_template('positive_response.html',probability=str(round(probability*100, 2)))
 
 
 def preprocessing(waveform_file):
 
     ''' Takes in a wave file and returns the mel frequency and the mel spectogram '''
     audio, sr = librosa.load(waveform_file)
+    waveform_file.stream.seek(0)
+    
+    ## Validate Cough
+    sr1, audio1 = wavfile.read(waveform_file)
+    prob_cough = validate_cough(audio1, sr1)
+
+    if prob_cough < 0.5:
+        raise ValueError("""The audio was not detected to be a cough. Please
+                          use a different file or re-record the cough.  
+                          Try to remove background noise if possible.""")
+
     mfccs = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=39)
     mfccs_scaled = np.mean(mfccs.T, axis=0)
     mfccs_1 = np.expand_dims(mfccs_scaled, axis=0)
@@ -117,12 +131,7 @@ def preprocessing(waveform_file):
     final = [mfccs_1,image_1,sample_2]
     return final
 
-<<<<<<< HEAD
 def validate_cough(audio, sr):
-    """
-    Takes in audio and sampling rate and returns probability of audio being
-    a cough.
-    """
     model_dir = '../data'
     with open (f'{model_dir}/cough_classifier', 'rb') as pickle_f:
         detection_model = pickle.load(pickle_f)
@@ -131,12 +140,9 @@ def validate_cough(audio, sr):
     prob_cough = classify_cough(audio, sr, detection_model, detection_scaler)
     print(f"The file has a {round(prob_cough*100, 2)}% probability of being a cough.")
     return prob_cough
-=======
-
->>>>>>> 673784c07b505b99ed5d159c6c79da4e26f47830
 
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
+    app.run(host='0.0.0.0', port=8081, threaded=True, debug=False)
